147ce9c488fce668142ec44bef8a99fdae27a0fd
kasahun-welela <kasahunwelela1@gmail.com>
2025-05-19 23:24:47 +0300

    remove unused import

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import * as React from "react";
-import {
-  AudioWaveform,
-  BookOpen,
-  Bot,
-  Command,
-  Frame,
-  GalleryVerticalEnd,
-  Map,
-  PieChart,
-  Settings2,
-  SquareTerminal,
-} from "lucide-react";
+import { Bot, SquareTerminal } from "lucide-react";
+
+// import {
+//   AudioWaveform,
+//   BookOpen,
+//   Bot,
+//   Command,
+//   Frame,
+//   GalleryVerticalEnd,
+//   Map,
+//   PieChart,
+//   Settings2,
+//   SquareTerminal,
+// } from "lucide-react";
 
 import { NavMain } from "@/components/nav-main";
 // import { NavProjects } from "@/components/nav-projects";
