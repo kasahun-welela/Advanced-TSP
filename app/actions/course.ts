1f2ff3a01374aa549863a30d74b840da6b8a4373
kasahun-welela <kasahunwelela1@gmail.com>
2025-05-29 22:38:18 +0300

    add interface

--- app/actions/course.ts
+++ app/actions/course.ts
@@ -1,30 +1,23 @@
 "use server";
 
+import { CreateCourse } from "@/interfaces";
 import axiosInstance from "@/lib/axios";
 
-export async function createCourse(formData: {
-  title: string;
-  description: string;
-  thumbnail: string;
-  logo_url: string;
-  price: number;
-  difficulty_level: string;
-  status: string;
-  duration_months: number;
-  course_type: string;
-  delivery_method: string;
-}) {
+export async function createCourse(formData: CreateCourse) {
   try {
     const res = await axiosInstance.post("/courses", formData);
     return {
       success: true,
-      data: res.data
+      data: res.data,   
     };
   } catch (error: any) {
     console.error("Create course error:", error);
     return {
       success: false,
-      error: error.response?.data?.message || error.message || "Failed to create course"
+      error:
+        error.response?.data?.message ||
+        error.message ||
+        "Failed to create course",
     };
   }
 }
