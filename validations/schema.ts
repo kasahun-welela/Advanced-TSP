2ccf905e311fc6386f5a374c585647e32b0b619b
kasahun-welela <kasahunwelela1@gmail.com>
2025-06-04 19:57:43 +0300

    update UI, and fetch topic in create class video

--- validations/schema.ts
+++ validations/schema.ts
@@ -26,3 +26,14 @@ export const createClassSchema = z.object({
   classTopic: z.string().min(6, "Class topic is required"),
   description: z.string().min(6, "Description is required"),
 });
+
+export const createClassVideoSchema = z.object({
+  courseTitle: z.string().min(1, "Course title is required"),
+  phaseName: z.string().min(1, "Phase name is required"),
+  weekName: z.string().min(1, "Week name is required"),
+  selectClass: z.string().min(1, "Class selection is required"),
+  classVideoTitle: z.string().min(1, "Video title is required"),
+  classVideoURL: z.string().min(1, "Video URL is required"),
+  classVideoLength: z.string().min(1, "Video length is required"),
+  disableVideo: z.boolean(),
+});
